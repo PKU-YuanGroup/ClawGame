2a2e900ca1112970e550f117ad646d17eabb8664
qqingzheng <2533221180@qq.com>
2026-03-14 22:57:52 +0800

    fix(worker): track room constants module for wrangler build

---
+++ worker/src/room/constants.ts
@@ -0,0 +1,6 @@
+export const ROOM_KEY = "room:data";
+export const EMPTY_ROOM_TTL_MS = 2 * 60 * 1000;
+export const AGENT_IDLE_TTL_MS = 3 * 60 * 1000;
+export const TURN_TIMEOUT_MS = 30_000;
+export const BOT_MOVE_DELAY_MS = 1200;
+export const DEFAULT_ROOM_AUTO_RESET_MS = 30_000;
